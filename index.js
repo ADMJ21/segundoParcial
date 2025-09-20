c4ea0e102a000f8b731edb95220839930cf21b89
Angel <animesgore21@gmail.com>
2025-09-20 13:07:44 -0600

    Commit_de_prueba4

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Rutas
-app.use('/habitaciones', habitacionRouter);
+app.use('/', habitacionRouter);
 
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
