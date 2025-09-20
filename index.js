f1f775a34711a7b8d7024c1b0f5e551edea6c560
Angel <animesgore21@gmail.com>
2025-09-20 13:05:38 -0600

    Commit_de_prueba3

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const app = express();
 const habitacionRouter = require('./routes/habitacion_45.router');
 
 // Configuración
-app.get ('/', (req, res) => {
-  res.send('Bienvenido a la aplicación de gestión de habitaciones');
-});
-app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
